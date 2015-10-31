79b397212d4f7e2c72505096a346ac6611020dd3
adsurang <11.adhar@gmail.com>
2015-10-31 21:33:18 +0530

    User Controller end to end

---
+++ Service/Server/BreakingBread/Service/UserService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BreakingBread.Service
+{
+    public class UserService
+    {
+
+        public IEnumerable<User> GetAllUsers()
+        {
+
+            List<User> users = new List<User>();
+            return users;
+        }
+
+        public User CreateUser(User user)
+        {
+            var component = new UserComponent();
+            component.AddUser(user);
+            return user;
+        }
+
+    }
+}
